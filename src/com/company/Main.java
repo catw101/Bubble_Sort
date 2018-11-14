c6e7cb1964e3f6807acb5448f4918f6976350346
catw101 <catelenwu@gmail.com>
2018-11-13 20:14:21 -0500

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Main {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
-    public static void main(String[] args) {
-	int[] runnerArray = {0, 1, 2, 3, 4, 5};
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j] > arr[i+1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
     }
 }
---
+++ src/com/company/Runner.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	int[] runnerArray = {0, 1, 2, 3, 4, 5};
+    }
+}
