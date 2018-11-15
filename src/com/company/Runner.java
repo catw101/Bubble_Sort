7985787e18f0d5af27bf0c4a95ee7d3fbddca5df
BT_1N3_27 <cwu8959@bths.edu>
2018-11-15 11:40:40 -0500

    update

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,14 +1,21 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Runner {
 
     public static void main(String[] args) {
         int[] something = Main.randInt(100);
+        int[] copy1 = Arrays.copyOf(something, something.length);
         Main.bubbleSort(something);
         Main.print(something);
         Main.selectionSort(something);
         Main.print(something);
         Main.insertSort(something);
         Main.print(something);
+
+        long time = System.currentTimeMillis();
+        time = System.currentTimeMillis() - time;
+        System.out.println("Insertion sort time took: " + time *.001 + "seconds.");
     }
 }
